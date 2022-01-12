645ab5e4b8c70cb987ef1a5bc5844bb0b09f52e5
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-01-13 02:19:40 +0700

    Setup prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,3 +6,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
+
+/**
+ * Install prettier extension (vscode) so it's easier to format your code.
+ * Press "shift + alt + f" for a single file or "ctrl + k ctrl + f".
+ *
+ * To check whether prettified or not, "prettier --check ./src"
+ * To fix and do a prettify, "prettier --write ./src"
+ *
+ * Or run via script, see on package.json
+ */
