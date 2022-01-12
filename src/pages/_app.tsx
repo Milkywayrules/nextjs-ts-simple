69df3c6bb6e78efc8f61f69a9d1577a8a5a7c232
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-01-13 02:20:20 +0700

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default MyApp
 
 /**
  * Install prettier extension (vscode) so it's easier to format your code.
- * Press "shift + alt + f" for a single file or "ctrl + k ctrl + f".
+ * Press "shift + alt + f" for a single file or "ctrl + k ctrl + f" for a single line.
  *
  * To check whether prettified or not, "prettier --check ./src"
  * To fix and do a prettify, "prettier --write ./src"
