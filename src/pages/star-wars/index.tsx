951b1862855a34f6a95af8ceca58308b9ed61b4b
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-01-14 14:24:53 +0700

    :lipstick: (starwars) provide link to see person detail page
    link to see person detail page in-app and on external site is now available

--- src/pages/star-wars/index.tsx
+++ src/pages/star-wars/index.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import EmptyMessage from '../../components/EmptyMessage'
 import ErrorMessage from '../../components/ErrorMessage'
@@ -12,6 +14,32 @@ const fetchPerson = async () => {
   const res = await fetch(`https://swapi.dev/api/people`)
   return await res.json()
 }
+
+/**
+ * Get slug from url last parameter.
+ *
+ * possible format:
+ * - 'https://swapi.dev/api/people/1/'
+ * - 'https://swapi.dev/api/people/1'
+ *
+ * @param str string from url that have a slug as last param
+ * @returns slug from url last param
+ */
+const getSlug = (str: string) => {
+  const [x, y] = str.split('/').slice(-2)
+
+  if (y) return y
+  return x
+}
+
+/**
+ * Build url with pathname and slug.
+ *
+ * @param pathname url pathname
+ * @param slug url slug
+ * @returns builded pathname + slug
+ */
+const buildUrl = (pathname: string, slug: string) => `${pathname}/${slug}`
 
 /**
  * Go to localhost:port/star-wars
@@ -36,12 +64,26 @@ const StarWars: NextPage = ({}) => {
   if (isError) return <ErrorMessage />
   if (!resData) return <EmptyMessage />
 
+  const router = useRouter()
+  const pathname = router.pathname
+
   return (
-    <div className="flex flex-col gap-4">
-      {resData.map(person => (
-        <PersonInfo key={person.url} person={person} />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-4">
+        {resData.map(person => (
+          <>
+            <PersonInfo key={person.url} person={person} />
+            <div>
+              <Link href={buildUrl(pathname, getSlug(person.url))}>
+                <a className="text-emerald-600 underline hover:text-emerald-500">
+                  {buildUrl(pathname, getSlug(person.url))}
+                </a>
+              </Link>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
   )
 }
 
